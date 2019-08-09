80a746c64eb6876e5d345b6454650a2d1945d693
seattleowl <seattleowl@users.noreply.github.com>
2019-08-09 13:17:27 -0700

    Change a few things

--- base/start.rb
+++ base/start.rb
@@ -1,6 +1,6 @@
 load "color.rb"
 
-puts "Thank you for downloading #{"ExecuteRuby!\n--PRESS ENTER--".pink}"
+puts "Thank you for downloading #{"ExecuteRuby".pink + "!" + "\n--PRESS ENTER--".pink}"
 
 gets.chomp
 load "main.rb"
